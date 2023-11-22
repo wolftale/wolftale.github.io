dd66d2a1e7960ef4487db4c7d5c370fb2ccd18ed
GP <gp@gpms.local>
2023-11-22 16:28:07 +0100

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://task-project.gtpas.repl.co/v1/tasks';
+  const apiRoot = 'https://task-project.gtpas.repl.co/v1/';
   const trelloApiRoot = 'https://task-project.gtpas.repl.co/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
